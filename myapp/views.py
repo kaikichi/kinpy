253a1e76aea1be731b914d9e2975393afcf29ab9
kaikichi <kinototo0147@gmail.com>
2018-05-27 23:19:25 +0900

    20180527

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse
  
 def index_template(request):
     myapp_data = {
-    'app': 'Django'
+    'app': 'Django',
+    'is_weekday': False,
     }
     return render(request, 'index.html', myapp_data)
